c12086559f9f053bdd20467fa7491185aa41869d
Ahsan Tariq <ahsantariq@Ahsans-Mac-mini.local>
2025-06-23 22:05:28 +1000

    feat: add SVG support with react-native-svg and transformer
    - Added react-native-svg and react-native-svg-transformer
    - Configured Metro bundler for SVG support
    - Added TypeScript declarations for SVG imports
    - Added Arrow.svg asset
    - Updated dependencies in package.json and bun.lock

--- metro.config.js
+++ metro.config.js
@@ -5,10 +5,21 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver = config.resolver || {};
-config.resolver.alias = {
-  ...(config.resolver.alias || {}),
-  '@': path.resolve(__dirname, 'src'),
+const { resolver, transformer } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...resolver,
+  alias: {
+    ...(resolver.alias || {}),
+    '@': path.resolve(__dirname, 'src'),
+  },
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
 };
 
 module.exports = withNativeWind(config, { input: './global.css' });
